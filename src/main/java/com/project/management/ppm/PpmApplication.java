d82b8002756aa367c29de8ed31605c8b42698c7c
rushabhoswal <rushabh123453@gmail.com>
2019-12-15 11:04:12 +0530

    Agile tool initial commit

---
+++ src/main/java/com/project/management/ppm/PpmApplication.java
@@ -0,0 +1,13 @@
+package com.project.management.ppm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/management/ppm/PpmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.management.ppm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

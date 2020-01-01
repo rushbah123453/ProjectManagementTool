3791e67315f98d81522f650776313340a9d2d0c1
rushabhoswal <rushabh123453@gmail.com>
2020-01-01 16:16:33 +0530

    heroku changes

--- src/main/java/com/project/management/ppm/constants/SecurityConstants.java
+++ src/main/java/com/project/management/ppm/constants/SecurityConstants.java
@@ -9,7 +9,7 @@ public final class SecurityConstants {
     public static final String SECRET="SecretKeyToGenJWT";
     public static final String TOKEN_PREFIX="Bearer "; // space is necessary after Bearer
     public static final String HEADER_STRING="Authorization";
-    public static final long EXPIRATION_TIME=30000; //30 seconds
+    public static final long EXPIRATION_TIME=3000000; //30 minutes
 
 
 }
